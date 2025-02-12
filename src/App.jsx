47ae1180c9e09976d8431c0fa44de4aad49b8136
sakshisinha-13 <118090835+sakshisinha-13@users.noreply.github.com>
2025-02-12 23:14:37 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>    
         <div className='outer-pokedex'>
           <h1 id="pokedex-heading">
-            <Link to="/">Pokedex</Link>
+            <Link to="/">POKEDEX</Link>
           </h1>
           <CustomRoutes />
         </div>
