9ad8700c2df1b02a487a88b5a3ec53659345977a
sakshisinha-13 <118090835+sakshisinha-13@users.noreply.github.com>
2025-02-12 23:16:56 +0530

    Update Pokemon.jsx

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Pokemon({ name, image, id }) {
     return (
         <div className="pokemon">
-            <Link to={`/pokemon/${id}`}>
+            <Link to={`/pokemon/${id}`}  style={{ textDecoration: 'none' }}>
                 <div className='pokemon-name'>{name}</div>
                 <img className="pokemon-image" src={image} alt={name} />
             </Link>
